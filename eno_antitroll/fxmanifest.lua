f2e2b99844f7c7dae8fb3007448c83011d8e39f6
Eno <90115512+DonEnes@users.noreply.github.com>
2024-04-21 19:47:53 +0200

    Update fxmanifest.lua

--- eno_antitroll/fxmanifest.lua
+++ eno_antitroll/fxmanifest.lua
@@ -2,7 +2,7 @@ fx_version "cerulean"
 game "gta5"
 
 author "Eno"
-description "Anti Troll System - Weapon & Vehicle Whitelist / coded by Eno"
+description "Anti Troll System - Weapon & Vehicle Whitelist by Eno"
 
 client_script "client.lua"
 shared_script "config.lua"
